1a9f3d0ac42e5b85c8b8a4ff08c1db34102766bd
Jasper Reed <jmixon.reed@gmail.com>
2020-07-20 18:21:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   planeteer_calls.count do |el|
     el.length <= 4
-    return boolean
+    return true
   end
 end
 
